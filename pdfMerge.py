36dfa5168eb3265658e6f6d6137e7a242fc62e5a
Sebas <juarezsebastian297@gmail.com>
2020-11-10 23:31:06 -0600

    Da error en la salida, ambos PDF en blanco

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,1 +1,28 @@
-hola esto es una prueba :) 
+# importing required modules
+import PyPDF2
+
+def PDFmerge(pdfs, output):
+	merger = PyPDF2.PdfFileMerger()
+
+	# appending pdfs one by one
+	for pdf in pdfs:
+		with open(pdf, 'rb') as f:
+			merger.append(f)
+
+	# writing combined pdf to output pdf file
+	with open(output, 'wb') as f:
+		merger.write(f)
+
+def main():
+	# pdf files to merge
+	pdfs = ['8vo.pdf', '10BACO.pdf']
+
+	# output pdf file name
+	output = 'combinado.pdf'
+
+	# calling pdf merge function
+	PDFmerge(pdfs = pdfs, output = output)
+
+if __name__ == "__main__":
+	# calling the main function
+	main()
